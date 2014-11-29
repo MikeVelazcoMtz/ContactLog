bee928ca5087ff84956603093dbbae4daca05078
Damián Pérez <dperez@netwarmonitor.com>
2014-11-28 19:04:54 -0600

    changes

--- contactLog/apps/contact/views.py
+++ contactLog/apps/contact/views.py
@@ -9,9 +9,26 @@ from .forms import ContactForm
 class ContactView(LoginRequiredMixin, FormView):
     template_name = 'pretty.html'
     form_class = ContactForm
-    success_url = 'contact/'
+    success_url = '/contact/'
 
     def get_context_data(self, **kwargs):
         context = super(ContactView, self).get_context_data(**kwargs)
         context['items'] = Contacts.objects.all()
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        success = False
+        if form.is_valid():
+            form.save()
+            success = True
+
+        return render(request, self.template_name, {'form': form, 'success': success})
+
+    def form_invalid(self, form_contact):
+        return self.render(
+            self.get_context_data(form_contact=form_contact,))
+
+    def form_valid(self, form):
+        return super(ContactView, self).form_valid(form)
